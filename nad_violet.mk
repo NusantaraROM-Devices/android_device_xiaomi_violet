e0e947b30f4699a318da4c938bef5b9cdd2dd200
kibria5 <106745675+kibria5@users.noreply.github.com>
2023-06-04 17:14:10 +0000

    violet: Nusantara Bringup

--- nad_violet.mk
+++ nad_violet.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2018-2020 The LineageOS Project
-# Copyright (C) 2020 The PixelExperience Project
+# Copyright (C) 2023 The NusantaraProject Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,15 +8,25 @@
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common Pixel Experience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_USES_AOSP_RECOVERY := true
+# Inherit some common NusantaraProject stuff
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+
+# MiuiCamera
+$(call inherit-product, vendor/MiuiCamera/config.mk)
+
+# Extra Flags
+NAD_BUILD_TYPE := OFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
-IS_PHONE := true
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Charging Animation
+USE_PIXEL_CHARGING := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_violet
+PRODUCT_NAME := nad_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
@@ -26,3 +36,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="violet"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_INCLUDE_APERTURE_CAMERA ?= false
